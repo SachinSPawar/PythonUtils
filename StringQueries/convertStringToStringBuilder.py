b2af8d195748a982b0153bc24f0b35e8e38ed58d
Sachin Pawar <SachinSPawar@users.noreply.github.com>
2016-08-11 12:16:00 +0530

    Create convertStringToStringBuilder.py

---
+++ StringQueries/convertStringToStringBuilder.py
@@ -0,0 +1,21 @@
+import re
+def parseToBuilder(queryString):
+
+    finalQuery = 'StringBuilder query=new StringBuilder();'
+
+    queryString=queryString.replace("query=","");
+    queryString=queryString.replace("query+=","");
+    queryParts = queryString.split(';');
+
+
+    queryTokens = []
+
+    for parts in queryParts:
+        parts = re.sub(r"([\'][\"][+][a-zA-Z]*[+][\"][\'])", '?', parts )
+        queryTokens.append(parts.split('+'))
+
+    for tokens in queryTokens:
+        for token in tokens:
+
+            finalQuery=finalQuery+"\n query.append( "+token+" );"
+    return finalQuery;
